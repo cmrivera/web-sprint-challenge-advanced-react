536c5936e4685b28846b1201b45cad8a5b8cb5b7
cmrivera <cmrivera0004@student.stcc.edu>
2020-06-13 14:12:14 -0400

    useForm called in Checkout form

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,21 +1,12 @@
+import useState from "react";
 // write your custom hook here to control your checkout form
-//import { useLocalStorage } from "./useLocalStorage";
-/*import useState from "react";
 
 export const useForm = (initialValues, key) => {
   const [values, setValues] = useState(initialValues, key);
 
   const handleChanges = (e) => {
-    console.log(e.target.name, e.target.value);
-    setValues({
-      ...values,
-      [e.target.name]: e.target.value,
-    });
+    setValues({ ...values, [e.target.name]: e.target.value });
   };
 
-  const clearForm = (e) => {
-    e.preventDefault();
-    setValues(initialValues);
-  };
-  return [values, clearForm, handleChanges];
-};*/
+  return [values, handleChanges];
+};
