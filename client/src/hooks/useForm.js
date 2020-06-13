eac26eacb0ced2649ee7c1e14973da24e04f14b4
cmrivera <cmrivera0004@student.stcc.edu>
2020-06-13 14:07:08 -0400

    useForm added

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,21 @@
 // write your custom hook here to control your checkout form
+//import { useLocalStorage } from "./useLocalStorage";
+/*import useState from "react";
+
+export const useForm = (initialValues, key) => {
+  const [values, setValues] = useState(initialValues, key);
+
+  const handleChanges = (e) => {
+    console.log(e.target.name, e.target.value);
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const clearForm = (e) => {
+    e.preventDefault();
+    setValues(initialValues);
+  };
+  return [values, clearForm, handleChanges];
+};*/
